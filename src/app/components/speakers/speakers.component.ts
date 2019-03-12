82114dcaf8f0daa9e211b9df91a553b7707572d5
azkarmoulana <azkar.moulana@gmail.com>
2019-03-12 18:13:21 +0530

    speaker model

--- src/app/components/speakers/speakers.component.ts
+++ src/app/components/speakers/speakers.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpeakersComponent implements OnInit {
 
-  speakers = ["Azkar Moulana", "Azkar Moulana", "Azkar Moulana", "Azkar Moulana", "Azkar Moulana", "Azkar Moulana", "Azkar Moulana", "Azkar Moulana", "Azkar Moulana", "Azkar Moulana", "Azkar Moulana", "Azkar Moulana","Azkar Moulana", "Azkar Moulana","Azkar Moulana", "Azkar Moulana",]
-
+  speakers = []
   constructor() { }
 
   ngOnInit() {
