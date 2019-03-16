006d2af2047562a17093992b2821a17179c359f0
azkarmoulana <azkar.moulana@gmail.com>
2019-03-16 20:24:30 +0530

    fixed routing error in prod build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,14 @@ import { NgModule } from '@angular/core';
 import { ComingSoonPageComponent } from './components/coming-soon-page/coming-soon-page.component';
 
 import { RouterModule, Routes } from '@angular/router';
-import { SponsorshipPackagesComponent } from './components/sponsorship-packages/sponsorship-packages.component';
-
-const Routes: Routes = [
-    { path: 'packages', component: SponsorshipPackagesComponent },
-]
 
 @NgModule({
 
     imports: [
-        RouterModule.forChild(Routes),
+        
     ],
     exports: [
-        RouterModule
+       
     ],
 })
 
